3bc53d913d6a317837014d565836d6109aac09f3
darkseid <uberdeveloper001@gmail.com>
2024-11-29 18:43:18 +0530

    Nim speed test updated with debug and release modes

--- nmath.nim
+++ nmath.nim
@@ -6,3 +6,12 @@ proc nim_fib(n: int): int {.exportpy.} =
     elif n < 3:
         return 1
     return nim_fib(n-1) + nim_fib(n-2)
+
+
+proc nim_sum(n:int): int {.exportpy.} =
+  # a naive to sum integers
+    var sum = 0
+    for i in 0..n:
+        sum += i
+    return sum
+
